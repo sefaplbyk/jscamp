43de9a375efa882505ecbae2afc3a50d0bbb2cee
Sefap <sefa__2095@hotmail.com>
2022-03-25 18:52:51 +0300

    25.03.2022 Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,54 @@
 console.log("Merhaba Kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
 
+//JS type safe değildir yani tip güvenliği yoktur başta ne tanımladıysan tip olarak öyle devam edilir
+let dolarBugun = 9.30
 let dolarDun = 9.20
 dolarDun = "9.20"
+
 {
+
     let dolarDun = 9.10
+
 }
 
 console.log(dolarDun)
-
+// const ile veri tanımlayınca o veriye yeni değer atanamaz
 const euroDun = 11.2
-//euroDun = 11
+// euroDun = 11 // bunu yapamıyoruz
 
 console.log(euroDun)
 
 //array
-//camelCasing
+//camelCasing 
 //PascalCasing
 let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+for(let i =0;i<konutKredileri.length;i++){
     console.log("<li>"+konutKredileri[i]+"</li>")
+
 }
 console.log("</ul>")
+{/*
+    <ul>
+        <li>Konut kredisi</li>
+        <li>Emlak konut kredisi </li>
+        <li>Kamu Konut Kredisi</li>
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
+    </ul>
+*/
+}
+
 
 console.log(konutKredileri)
+
+
+
+
+
+
+
+
+
+
